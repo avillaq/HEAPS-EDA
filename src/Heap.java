472277a6c00ace6df2030481a2ec710fa650b188
Alexander Villafuerte Quispe <villafuertequispealex@gmail.com>
2023-06-16 12:04:52 -0500

    Implementacion del metodo insert() completado (Sin probar)

--- src/Heap.java
+++ src/Heap.java
@@ -1,14 +1,20 @@
 import java.util.ArrayList;
 
-public class Heap<T> {
+public class Heap<T extends Comparable<T>> {
     private ArrayList<T> array;
     public Heap(){
         array = new ArrayList<T>();
    
     }
-    public void insert(T item, T[] a) {
+    public void insert(T item) {
         array.add(item);
         T temp;
+        for(int j=array.size(); j>1 && array.get(j).compareTo(array.get(j/2))>0; j/=2) {
+            temp = array.get(j);
+            array.set(j, array.get(j/2));
+            array.set(j/2, temp);
+
+        }
 
     }
 }
