79cfa227dd1c69fdfe6b35c9839d2edd4b972373
avillaq <villafuertequispealex@gmail.com>
2023-06-17 03:15:24 -0500

    Se probaron los metodos en la clase TestPriority

--- src/TestPriority.java
+++ src/TestPriority.java
@@ -1,5 +1,16 @@
 public class TestPriority {
     public static void main(String[] args) {
-        
+        PriorityQueueHeap<Integer> queue = new PriorityQueueHeap<Integer>();
+        queue.enqueue(10, 1);
+        queue.enqueue(5, 4);
+        queue.enqueue(15, 10);
+        queue.enqueue(8, 2);
+        queue.enqueue(12, 3);
+        queue.enqueue(4, 7);
+
+        System.out.println("Front: " + queue.front());  
+        System.out.println("Back: " + queue.back());    
+        System.out.println("Elemento elimininado: " + queue.dequeue());
+
     }
 }
