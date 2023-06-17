b3262625dcf12caf4bcc726d9e08241e49cfc0c1
avillaq <villafuertequispealex@gmail.com>
2023-06-16 23:25:48 -0500

    Modificacion en el metodo insert(). Ya funciona completamente

--- src/Heap.java
+++ src/Heap.java
@@ -9,10 +9,11 @@ public class Heap<T extends Comparable<T>> {
     public void insert(T item) {
         array.add(item);
         T temp;
-        for(int j=array.size()-1; j>0 && array.get(j).compareTo(array.get(j/2))>0; j/=2) {
+
+        for(int j=array.size()-1; j>0 && array.get(j).compareTo(array.get((j-1)/2))>0; j=(j-1)/2) {
             temp = array.get(j);
-            array.set(j, array.get(j/2));
-            array.set(j/2, temp);
+            array.set(j, array.get((j-1)/2));
+            array.set((j-1)/2, temp);
 
         }
 
--- src/Test.java
+++ src/Test.java
@@ -1,16 +1,23 @@
 public class Test {
     public static void main(String[] args) {
         Heap<Integer> heap = new Heap<Integer>();
+        
         heap.insert(10);
-        heap.insert(20);
-        heap.insert(30);
-        heap.insert(40);
-        heap.insert(50);
-        heap.insert(60);
-        heap.insert(70);
-        heap.insert(80);
-
-
         System.out.println(heap);
+        heap.insert(20);
+        System.out.println(heap);
+        heap.insert(30);
+        System.out.println(heap);
+        heap.insert(40);
+        System.out.println(heap);
+        heap.insert(50);
+        System.out.println(heap);
+        heap.insert(60);
+        System.out.println(heap);
+        heap.insert(70);
+        System.out.println(heap);
+        heap.insert(80);
+System.out.println(heap);
+
     }
 }
