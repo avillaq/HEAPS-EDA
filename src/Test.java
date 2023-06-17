c473c3df529a9d6f3b388b84042a628a2c55053b
avillaq <villafuertequispealex@gmail.com>
2023-06-17 01:06:23 -0500

    El metodo remove() es completamente funcional

--- src/Test.java
+++ src/Test.java
@@ -1,23 +1,35 @@
 public class Test {
     public static void main(String[] args) {
         Heap<Integer> heap = new Heap<Integer>();
-        
-        /* heap.insert(16);
-        heap.insert(14);
-        heap.insert(10);
-        heap.insert(8);
-        heap.insert(7);
-        heap.insert(9);
-        heap.insert(3);
-        heap.insert(2);
-        heap.insert(15); */
-        heap.insert(10);
-        heap.insert(20);
-        heap.insert(30);
-        heap.insert(40);
+
+        /*
+         * heap.insert(16);
+         * heap.insert(14);
+         * heap.insert(10);
+         * heap.insert(8);
+         * heap.insert(7);
+         * heap.insert(9);
+         * heap.insert(3);
+         * heap.insert(2);
+         * heap.insert(15);
+         */
+        /*
+         * heap.insert(10);
+         * heap.insert(20);
+         * heap.insert(30);
+         * heap.insert(40);
+         * heap.insert(50);
+         * heap.insert(60);
+         * heap.insert(70);
+         * heap.insert(80);
+         */
         heap.insert(50);
-        heap.insert(60);
+        heap.insert(30);
+        heap.insert(20);
+        heap.insert(40);
         heap.insert(70);
+        heap.insert(60);
+        heap.insert(90);
         heap.insert(80);
 
         heap.remove();
