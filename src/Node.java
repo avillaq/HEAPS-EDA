e062a1101e22776e7720c65eb7d95e9c8cc4ff9b
avillaq <villafuertequispealex@gmail.com>
2023-06-17 02:15:23 -0500

    La clase node implementara Comparable para compararlos por su prioridad

--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,15 @@
-public class Node {
-    
+public class Node<T extends Comparable<T>> implements Comparable<T> {
+    private T data;
+    private int priority;
+
+    public Node(T data, int priority) {
+        this.data = data;
+        this.priority = priority;
+    }
+
+    @Override
+    public int compareTo(T elem) {
+        
+    }
+        
 }
